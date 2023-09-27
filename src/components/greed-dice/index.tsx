57023a95c754bd6ecbd4273d3071f7785481af8a
Mathieu <studio.creatic@gmail.com>
2023-09-27 12:10:28 +0200

    ajout du score de 1000 point, et un dé minimum par tour

--- src/components/greed-dice/index.tsx
+++ src/components/greed-dice/index.tsx
@@ -15,6 +15,9 @@ function Greed() {
   const initialPlayerScores = Array(numPlayers).fill(0);
   const [playerScores, setPlayerScores] = useState<number[]>(initialPlayerScores);
   const [activePlayerScore, setActivePlayerScore] = useState<number>(0);
+  const [firstTurn, setFirstTurn] = useState<boolean>(true);
+  const [hasSavedDiceThisTurn, setHasSavedDiceThisTurn] = useState<boolean>(false);
+
 
   const checkWin = () => {
     if(activePlayerScore >= 10000){
@@ -43,14 +46,18 @@ function Greed() {
 
   // Gestionnaire de clic pour passer au joueur suivant
   const handleNextPlayer = () => {
-    if (result === 0) {
-      setActivePlayerScore(0);
+    if (activePlayerScore >= 1000 || playerScores[currentPlayerIndex] >= 1000) {
+      if (activePlayerScore  === 0) {
+        setActivePlayerScore(0);
+      } else {
+        setPlayerScores((prevScores) => {
+          const newScores = [...prevScores];
+          newScores[currentPlayerIndex] += activePlayerScore;
+          return newScores;
+        });
+      }
     } else {
-      setPlayerScores((prevScores) => {
-        const newScores = [...prevScores];
-        newScores[currentPlayerIndex] += activePlayerScore;
-        return newScores;
-      });
+      alert("Vous devez atteindre au moins 1000 points pour commencer à comptabiliser vos points.");
     }
     
     setDice([]);
@@ -60,15 +67,6 @@ function Greed() {
     setActivePlayerScore(0);
     checkWin();
   };
-
-  const nextPlayer = () => {
-    setDice([]);
-    setSavedDice([]);
-    setCurrentPlayerIndex((prevIndex) => (prevIndex + 1) % numPlayers);
-    setShowSaveButton(false);
-    setActivePlayerScore(0);
-    checkWin();
-  }
 
   // Gestionnaire de clic pour sauvegarder un chiffre
   const handleSaveDice = (number: number) => {
@@ -80,6 +78,7 @@ function Greed() {
       newDice.splice(index, 1);
       setDice(newDice);
     }
+    setHasSavedDiceThisTurn(true);
   };
 
   // Gestionnaire de clic pour faire revenir un chiffre dans dice
@@ -88,22 +87,25 @@ function Greed() {
     // Trouver et retirer le chiffre de savedNumbers
     const index = savedDice.indexOf(number);
     if (index !== -1) {
-      const newSaveDice = [...savedDice];
-      newSaveDice.splice(index, 1);
-      setSavedDice(newSaveDice);
+      const newSaveDice = [...savedDice]
+      newSaveDice.splice(index, 1)
+      setSavedDice(newSaveDice)
     }
   };
   
   // Gestionnaire de clic pour ajouter 6 lancers de dé au tableau
   const handleRollDice = () => {
-    let shouldResetDiceAndScore = false;
+    let shouldResetDiceAndScore = false
 
+    // Vérifier si le joueur a sauvegardé au moins un dé ce tour
+    if (!hasSavedDiceThisTurn && !firstTurn) {
+      alert("Vous devez sauvegarder au moins un dé avant de lancer les dés.");
+    } else {
       if(dice.length === 0) {
-        const newDice = [];
+        const newDice = []
         for (let i = 0; i < 6; i++) {
-          newDice.push(getRandomDiceRoll());
+          newDice.push(getRandomDiceRoll())
         }
-
         // Mettre à jour les dés
         setDice(newDice);
         console.log(activePlayerScore)
@@ -113,27 +115,30 @@ function Greed() {
         if (result === 0) {
           alert("Dommage, pas un seul bon dé")
           setActivePlayerScore(0);
-          shouldResetDiceAndScore= true;
+          shouldResetDiceAndScore= true
         } 
-        setShowSaveButton(true);
-
+        setShowSaveButton(true)
+        setHasSavedDiceThisTurn(false)
+        setFirstTurn(false);
       }else {
         const newDice = [...dice];
         for (let i = 0; i < newDice.length ; i++) {
           newDice[i] = getRandomDiceRoll()
         }
-
         // Vérifier si c'est un Game Over
         const result = calculateFinalScore(newDice);
         console.log('result', result)
         if (result === 0) {
           // Game Over
           alert("Echec ! Dommage vous y étiez presque")
-          setActivePlayerScore(0);
-          shouldResetDiceAndScore= true;
+          setActivePlayerScore(0)
+          shouldResetDiceAndScore= true
         }
         setDice(newDice)
+        setHasSavedDiceThisTurn(false)
+        setFirstTurn(false);
       }
+    }
       
     if (shouldResetDiceAndScore) {
       setDice([]);
@@ -184,6 +189,9 @@ function Greed() {
   console.log('playerScores',playerScores)
   console.log('activePlayerScore',activePlayerScore)
 
+  console.log('playerScores',playerScores)
+  console.log('activePlayerScore',activePlayerScore)
+
   return (
     <div>
       {/* Sélection du nombre de joueurs */}
@@ -227,7 +235,7 @@ function Greed() {
               Final Score : {activePlayerScore}
             </div>
           </div>
-          <button onClick={handleNextPlayer}>Joueur suivant</button>
+          <button onClick={handleNextPlayer}>Finir mon tour</button>
           <div>
             {playerNames.map((playerName, index) => {
               if (index !== currentPlayerIndex) {
@@ -242,62 +250,45 @@ function Greed() {
           </div>
         </div>
       )}
+    <div id="greedRules">
+      <h2>Règles du jeu deu 10</h2>
+      <h3>But du jeu :</h3>
+      <p>Atteindre un score de 10 000 points.</p>
+      <h2>Déroulement du jeu :</h2>
+      <p>Les joueurs jouent les uns après les autres.</p>
+      <h3>Description d'un tour de jeu :</h3>
+      <ol>
+        <li>Jeter les 5 dés.</li>
+        <li>Mettre de côté le ou les dés permettant de gagner des points.</li>
+        <li>Si on le souhaite arrêter, laisser la main au joueur suivant et additionner le score obtenu lors du tour aux points déjà acquis lors des autres tours.</li>
+        <li>Ou si on préfère lancer les dés non utilisés dans l'espoir de faire grimper le score obtenu lors du tour.</li>
+        <li>Si l'un des jets de dés ne rapporte rien, le score du tour du jeu est nul, et c'est au joueur suivant de lancer les 5 dés.</li>
+      </ol>
+      <h3>Comptage des points :</h3>
+      <ul>
+        <li>Un As : 100 pts</li>
+        <li>Un 5 : 50 pts</li>
+        <li>Brelan d'As : 1000 pts</li>
+        <li>Brelan de 5 : 500 pts</li>
+        <li>Brelan de 2, 3, 4, 6 : 200, 300, 400, 600 pts</li>
+        <li>Suite : 1, 2, 3, 4, 5 ou 2, 3, 4, 5, 6 : 1500 pts</li>
+      </ul>
+      <h4>Remarque 1 :</h4>
+      <p>Pour être valides, les brelans ou suites doivent être produits sur un seul jeté de dé. Exemple :</p>
+      <ol>
+        <li>On obtient 1, 3, 4, 4, 6 au premier lancé : Seul le 1 permet d'augmenter le score du tour (+100 pts). On le garde et on relance les autres dés.</li>
+        <li>On obtient 5, 3, 3, 4 : Seul le 5 permet d'augmenter le score du tour (+50 pts, soit 150 au total). On le garde et on relance les autres dés.</li>
+        <li>On obtient 2, 3, 4 : Rien ne permet de marquer avec ces 3 dés (même si les 5 dés constituent une suite), les 150 points sont perdus.</li>
+      </ol>
+      <h4>Remarque 2 :</h4>
+      <p>Lorsque les 5 dés ont permis de marquer, ils peuvent tous être relancés en vue d'améliorer le score du tour (au risque de le perdre entièrement).</p>
+      <h4>Remarque 3 :</h4>
+      <p>Il faut totaliser 1000 points avant de pouvoir commencer à comptabiliser ses points.</p>
 
+    </div>
   </div>
   );
 }
 
 export default Greed;
 
-
-
-{/* <div id="greedRules">
-<p>
-    But du jeu :
-    Atteindre un score de 10 000 points.
-    Déroulement du jeu :
-    Les joueurs jouent les un après les autres.
-    Description d'un tour de jeu :
-    Jeter les 5 dés.
-    Mettre de côté le ou les dés permettant de gagner des points.
-    Si on le souhaite arrêter, laisser la main au joueur suivant et additionner le score obtenu lors du tour aux points déjà acquis lors des autres tours.
-    Ou si on préfère lancer les dés non utilisés dans l'espoir de faire grimper le score obtenu lors du tour.
-    Si un des jets de dés ne rapporte rien, le score du tour du jeu est nul, et c'est au joueur suivant de lancer les 5 dés.
-    Comptage des points :
-    Un As : 100 pts
-    Un 5 : 50 pts
-    Brelan d'As : 1000 pts
-    Brelan de 5 : 500 pts
-    Brelan de 2,3,4,6 : 200, 300, 400,600 pts
-    Suite : 1,2,3,4,5 ou 2,3,4,5,6 : 1500 pts
-    Remarque 1 : Pour être valides, les brelans ou suites doivent être produits sur un seul jeté de dé.
-    Ex :
-    on obtient 1, 3, 4, 4, 6 au premier lancé
-    seul le 1 permet d'augmenter le score du tour (+100 pts). On le garde et on relance les autres dés.
-    On obtient 5, 3, 3, 4
-    seul le 5 permet d'augmenter le score du tour (+50 pts, soit 150 en tout). On le garde et on relance les autres dés.
-    On obtient 2, 3, 4 : rien ne permet de marquer avec ces 3 dés (même si les 5 dés constituent une suite), les 150 points sont perdus.
-    Remarque 2 : Lorsque les 5 dés ont permis de marquer, ils peuvent être tous relancés en vue d'améliorer le score du tour (au risque de le perdre entièrement).
-    Remarque 3 : Il faut totaliser 1000 points avant de pouvoir commencer à comptabiliser ses points
-</p>
-</div> */}
-// Greed is a dice game played with five six-sided dice. Your mission, should you choose to accept it, is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
-
-//  Three 1's => 1000 points
-//  Three 6's =>  600 points
-//  Three 5's =>  500 points
-//  Three 4's =>  400 points
-//  Three 3's =>  300 points
-//  Three 2's =>  200 points
-//  One   1   =>  100 points
-//  One   5   =>   50 point
-// A single die can only be counted once in each roll. For example, a given "5" can only count as part of a triplet (contributing to the 500 points) or as a single 50 points, but not both in the same roll.
-
-// Example scoring
-
-//  Throw       Score
-//  ---------   ------------------
-//  5 1 3 4 1   250:  50 (for the 5) + 2 * 100 (for the 1s)
-//  1 1 1 3 1   1100: 1000 (for three 1s) + 100 (for the other 1)
-//  2 4 4 5 4   450:  400 (for three 4s) + 50 (for the 5)
-// In some languages, it is possible to mutate the input to the function. This is something that you should never do. If you mutate the input, you will not be able to pass all the tests.
