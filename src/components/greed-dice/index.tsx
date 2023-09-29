f20fdc0e225ad3ba5c81980beea55604f8cdc0c4
Mathieu <studio.creatic@gmail.com>
2023-09-29 12:57:33 +0200

    Ajout de la suite à l'algorithme, OK

--- src/components/greed-dice/index.tsx
+++ src/components/greed-dice/index.tsx
@@ -2,21 +2,21 @@ import { useState } from "react";
 import { getRandomDiceRoll } from "../../utils/diceRoll6";
 
 function Greed() {
-  const [dice, setDice] = useState<number[]>([]);
-  const [savedDice, setSavedDice] = useState<number[]>([]);
-  const [showSaveButton, setShowSaveButton] = useState<boolean>(false);
-  const [numPlayers, setNumPlayers] = useState<number>(2); 
-  const [currentPlayerIndex, setCurrentPlayerIndex] = useState<number>(0);
-  const [gameStarted, setGameStarted] = useState<boolean>(false);
-  const [gameOver, setGameOver] = useState<boolean>(false);
+  const [dice, setDice] = useState<number[]>([])
+  const [savedDice, setSavedDice] = useState<number[]>([])
+  const [showSaveButton, setShowSaveButton] = useState<boolean>(false)
+  const [numPlayers, setNumPlayers] = useState<number>(2);
+  const [currentPlayerIndex, setCurrentPlayerIndex] = useState<number>(0)
+  const [gameStarted, setGameStarted] = useState<boolean>(false)
+  const [gameOver, setGameOver] = useState<boolean>(false)
 
-  const [playerNames, setPlayerNames] = useState<string[]>(["Joueur 1", "Joueur 2", "Joueur 3", "Joueur 4"]);
+  const [playerNames, setPlayerNames] = useState<string[]>(["Joueur 1", "Joueur 2", "Joueur 3", "Joueur 4"])
 
-  const initialPlayerScores = Array(numPlayers).fill(0);
-  const [playerScores, setPlayerScores] = useState<number[]>(initialPlayerScores);
-  const [activePlayerScore, setActivePlayerScore] = useState<number>(0);
-  const [firstTurn, setFirstTurn] = useState<boolean>(true);
-  const [hasSavedDiceThisTurn, setHasSavedDiceThisTurn] = useState<boolean>(false);
+  const initialPlayerScores = Array(numPlayers).fill(0)
+  const [playerScores, setPlayerScores] = useState<number[]>(initialPlayerScores)
+  const [activePlayerScore, setActivePlayerScore] = useState<number>(0)
+  const [firstTurn, setFirstTurn] = useState<boolean>(true)
+  const [hasSavedDiceThisTurn, setHasSavedDiceThisTurn] = useState<boolean>(false)
 
 
   const checkWin = () => {
@@ -141,39 +141,95 @@ function Greed() {
     }
       
     if (shouldResetDiceAndScore) {
-      setDice([]);
-      setSavedDice([]);
-      setShowSaveButton(false);
-      handleNextPlayer();
+      setDice([])
+      setSavedDice([])
+      setShowSaveButton(false)
+      handleNextPlayer()
     }
   };
 
   // Fonction pour calculer le score en utilisant les chiffres sauvegardés
   const calculateFinalScore = (dice: number[]) => {
     const counts = [0, 0, 0, 0, 0, 0]
-    //score de base
-    let totalScore = 0;
+    let totalScore = 0
+  
     for (const value of dice) {
-      counts[value - 1]++;
-  }
-
-  //algorithme de calcul
-  for (let i = 0; i < 6; i++) {
-    if (counts[i] >= 3) {
-      if (i === 0) {
-        totalScore += 1000 * (counts[i] - 2);
-      } else {
-        totalScore += (i + 1) * 100 * (counts[i] - 2);
-      }
-      counts[i] -= 3;
+      counts[value - 1]++
     }
-  }
+  
+    console.log('dice', dice)
+    // Vérifier si le lancer de dés forme une suite
+    const uniqueDiceValues = Array.from(new Set(dice));
+    const sortedDice = uniqueDiceValues.sort((a, b) => a - b)
+    console.log('sortedDice', sortedDice)
 
-  // Ajouter le score pour les 1 et les 5 restants
-  totalScore += counts[0] * 100 + counts[4] * 50;
+    
+    const isStraight = () => {
+      if (sortedDice.length !== 5) {
+        return false;
+      } else {
+        return sortedDice[4] - sortedDice[0] === 4;
+      }
+    };
+
+    // Vérifier si le joueur a obtenu une suite
+    let gotStraight = false;
+    const remainingDice = [...dice];
+
+    if (isStraight()) {
+      gotStraight = true;
+      // Le joueur a obtenu une suite, attribuer 1000 points et marquer le drapeau
+      totalScore += 1000;
+     // Calculer le score pour les dés restants
+      const nonStraightDice = dice.filter((value) => !sortedDice.includes(value));
+      const ones = nonStraightDice.filter((value) => value === 1);
+      totalScore += ones.length * 100;
+      const fives = nonStraightDice.filter((value) => value === 5);
+      totalScore += fives.length * 50;
+    } else {
+      for (let i = 0; i < 6; i++) {
+        if (counts[i] === 3) {
+          if (i === 0) {
+            totalScore += 1000 * (i + 1) * (counts[i] - 2);
+          } else {
+            totalScore += (i + 1) * 100 * (counts[i] - 2);
+          }
+          counts[i] -= 3;
+        } else if (counts[i] === 4) {
+          if (i === 0) {
+            totalScore += 1000 * (i + 1) * 2;
+          } else {
+            totalScore += (i + 1) * 100 * 2;
+          }
+          counts[i] -= 4;
+        } else if (counts[i] === 5) {
+          if (i === 0) {
+            totalScore += 1000 * (i + 1) * 4;
+          } else {
+            totalScore += (i + 1) * 100 * 4;
+          }
+          counts[i] -= 5;
+        } else if (counts[i] === 6) {
+          if (i === 0) {
+            totalScore += 1000 * (i + 1) * 8;
+          } else {
+            totalScore += (i + 1) * 100 * 6;
+          }
+          counts[i] -= 6;
+        }
+      }
+      // Ajouter le score pour les 1 et les 5 restants
+      if (counts[0] > 0) {
+        totalScore += counts[0] * 100;
+      }
+      if (counts[4] > 0) {
+        totalScore += counts[4] * 50;
+      }
+      }
+
     return totalScore;
   };
-
+  
   // Calculer le score du lancer de dés
   const result = calculateFinalScore(dice);
 
@@ -185,9 +241,6 @@ function Greed() {
     const scoreToAddLater = activePlayerScore + calculatedFinalScore;
     setActivePlayerScore(scoreToAddLater);
   };
-
-  console.log('playerScores',playerScores)
-  console.log('activePlayerScore',activePlayerScore)
 
   console.log('playerScores',playerScores)
   console.log('activePlayerScore',activePlayerScore)
@@ -259,7 +312,7 @@ function Greed() {
       <h3>Description d'un tour de jeu :</h3>
       <ol>
         <li>Jeter les 5 dés.</li>
-        <li>Mettre de côté le ou les dés permettant de gagner des points.</li>
+        <li>Mettre de côté le ou les dés permettant de gagner des points et toujours au minimum un dés</li>
         <li>Si on le souhaite arrêter, laisser la main au joueur suivant et additionner le score obtenu lors du tour aux points déjà acquis lors des autres tours.</li>
         <li>Ou si on préfère lancer les dés non utilisés dans l'espoir de faire grimper le score obtenu lors du tour.</li>
         <li>Si l'un des jets de dés ne rapporte rien, le score du tour du jeu est nul, et c'est au joueur suivant de lancer les 5 dés.</li>
@@ -271,7 +324,10 @@ function Greed() {
         <li>Brelan d'As : 1000 pts</li>
         <li>Brelan de 5 : 500 pts</li>
         <li>Brelan de 2, 3, 4, 6 : 200, 300, 400, 600 pts</li>
-        <li>Suite : 1, 2, 3, 4, 5 ou 2, 3, 4, 5, 6 : 1500 pts</li>
+        <li>Carré de 2, 3, 4, 6 : 200*2, 300*2, 400*2, 600*2 pts</li>
+        <li>5 dés identique de 2, 3, 4, 6 : 200*4, 300*4, 400*4, 600*4 pts</li>
+        <li>6 dés identique de 2, 3, 4, 6 : 200*6, 300*6, 400*6, 600*6 pts</li>
+        <li>Suite : 1, 2, 3, 4, 5 ou 2, 3, 4, 5, 6 : 1000 pts</li>
       </ul>
       <h4>Remarque 1 :</h4>
       <p>Pour être valides, les brelans ou suites doivent être produits sur un seul jeté de dé. Exemple :</p>
